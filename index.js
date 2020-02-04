0d11c7412c670d06a8215aa949e3b3405ed6ef75
Andrew Dalmedo <andrewdalmedo@gmail.com>
2020-02-04 04:27:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ function updateObjectWithKeyAndValue(object, key, value) {
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
 }
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = object;
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+}
